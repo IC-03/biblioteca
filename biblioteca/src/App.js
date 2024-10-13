a3337b56a655e64a92a337cb2c12a14abc09ec0b
Isabella Callejas <isabella.callejas.m@gmail.com>
2024-10-13 15:12:15 -0500

    paginas y componentes

--- biblioteca/src/App.js
+++ biblioteca/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import MakeAppointment from './pages/Make_Appointment';
+import Login from './pages/Login';
+import Updatelinks from './pages/Updatelinks';
+import SeeAppointments from './pages/See_Apointments';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/cita' exact element={<MakeAppointment/>} />
+          <Route path='/ver-citas' exact element={<SeeAppointments/>}/>
+          <Route path='/iniciar-sesion' exact element={<Login/>} />
+          <Route path='/actualizar-links' exact element={<Updatelinks/>} />
+        </Routes>
+      </Router>
+    </Fragment>
   );
 }
 
